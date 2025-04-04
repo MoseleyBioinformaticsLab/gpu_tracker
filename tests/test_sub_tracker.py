a64d4423a5f58ebbc6f290bc6fb64b642bed6a3e
erikhuck <erik.huckvale2017@gmail.com>
2025-04-04 12:29:48 -0400

    Enables use of SubTracker as a function decorator

--- tests/test_sub_tracker.py
+++ tests/test_sub_tracker.py
@@ -34,8 +34,53 @@ def test_sub_tracker(mocker, code_block_name: str | None, sub_tracking_file: str
             assert val.endswith(default_code_block_end)
         else:
             assert val == code_block_name
-    expected_tracking_file = f'tests/data/{code_block_name}_{sub_tracking_file}.csv'
     utils.test_tracking_file(
-        actual_tracking_file=sub_tracker.sub_tracking_file, expected_tracking_file=expected_tracking_file,
+        actual_tracking_file=sub_tracker.sub_tracking_file, expected_tracking_file='tests/data/sub-tracker.csv',
         excluded_col='code_block_name', excluded_col_test=code_block_name_test
     )
+
+
+@pt.fixture(name='code_block_attribute', params=['my-attribute', None])
+def get_code_block_attribute(request):
+    yield request.param
+
+
+def test_decorator(mocker, code_block_name: str | None, code_block_attribute: str | None):
+    @gput.sub_track(code_block_name=code_block_name, code_block_attribute=code_block_attribute)
+    def decorated_function(arg1: int, arg2: int, kwarg1: int = 1, kwarg2: int = 2) -> int:
+        return arg1 + arg2 - (kwarg1 + kwarg2)
+    getpid_mock = mocker.patch('gpu_tracker.sub_tracker.os.getpid', return_value=1234)
+    n_iterations = 3
+    time_mock = mocker.patch('gpu_tracker.sub_tracker.time', time=mocker.MagicMock(side_effect=range(n_iterations * 2 * 2 + 2)))
+    for _ in range(n_iterations):
+        return_val = decorated_function(2, 5)
+        assert return_val == 4
+        return_val = decorated_function(3, 2, kwarg1=2, kwarg2=1)
+        assert return_val == 2
+    assert len(getpid_mock.call_args_list) == n_iterations * 2
+    assert len(time_mock.time.call_args_list) == n_iterations * 2 * 2
+
+    def code_block_name_test(val):
+        if code_block_name is None:
+            if code_block_attribute is None:
+                assert val.endswith('test_sub_tracker.py:decorated_function')
+            else:
+                assert val.endswith('test_sub_tracker.py:my-attribute')
+        else:
+            assert val == code_block_name
+    utils.test_tracking_file(
+        actual_tracking_file='1234.csv', expected_tracking_file=f'tests/data/decorated-function.csv',
+        excluded_col='code_block_name', excluded_col_test=code_block_name_test
+    )
+    if code_block_name is None and code_block_attribute is None:
+        return_val = utils.function_in_other_file(1, 2, 3, kw1=4, kw2=5)
+        assert return_val == ((1, 2, 3), {'kw1': 4, 'kw2': 5})
+        assert len(getpid_mock.call_args_list) == n_iterations * 2 + 1
+        assert len(time_mock.time.call_args_list) == n_iterations * 2 * 2 + 2
+
+        def code_block_name_test(val):
+            assert val.endswith('utils.py:function_in_other_file')
+        utils.test_tracking_file(
+            actual_tracking_file='1234.csv', expected_tracking_file='tests/data/decorated-function-other-file.csv',
+            excluded_col='code_block_name', excluded_col_test=code_block_name_test
+        )
